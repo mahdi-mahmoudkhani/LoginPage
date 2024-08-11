195eee427250d47b1de273eebb23a6d9ff58dbd7
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-08-11 19:35:15 +0330

    feat: Add register message UILabels with attributed text
    - Implemented loadRegisterMessage method to configure and add two UILabels for registration message
    - Added attributed text to emphasize "Register here !" with custom color and font
    - Set up constraints for both UILabels

--- LoginPageApp/ViewController.swift
+++ LoginPageApp/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
         
         loadAppLogo()
         loadSignInTitle()
+        loadRegisterMessage()
     }
     
     private func loadAppLogo() {
@@ -58,6 +59,40 @@ class ViewController: UIViewController {
             SignInTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 102),
             SignInTitle.widthAnchor.constraint(equalToConstant: 128),
             SignInTitle.heightAnchor.constraint(equalToConstant: 45),
+            
+        ] )
+    }
+    
+    private func loadRegisterMessage() {
+        
+        let registerMessageL1 = UILabel()
+        let registerMessageL2 = UILabel()
+        let stringText = "You can   Register here !"
+        let attributedString = NSMutableAttributedString(string: stringText)
+        let registerWordRange = (stringText as NSString).range(of: "Register here !")
+        
+        registerMessageL1.translatesAutoresizingMaskIntoConstraints = false
+        registerMessageL2.translatesAutoresizingMaskIntoConstraints = false
+        
+        registerMessageL1.text = "If you don’t have an account register"
+        registerMessageL1.font = UIFont(name: "Poppins-Regular", size: 16)
+        
+        attributedString.addAttribute(.foregroundColor, value: UIColor(red: 255 / 255, green: 67 / 255, blue: 42 / 255, alpha: 1.0), range: registerWordRange)
+        attributedString.addAttribute(.font, value: UIFont(name: "Poppins-SemiBold", size: 16)!, range: registerWordRange)
+        registerMessageL2.attributedText = attributedString
+        
+        self.view.addSubview(registerMessageL1)
+        self.view.addSubview(registerMessageL2)
+        
+        NSLayoutConstraint.activate( [
+            
+            registerMessageL1.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 28.48),
+            registerMessageL1.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 169),
+            registerMessageL1.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -31),
+            
+            registerMessageL2.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 28.48),
+            registerMessageL2.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 199),
+            registerMessageL2.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -31),
             
         ] )
     }
