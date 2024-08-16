1b6f9cf02ce2d313089a509853ae06ca607e3511
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-08-16 19:04:46 +0330

    feat(keyboard): Add keyboard handling to adjust view when text fields are covered
    - Implemented `setupKeyboardHiding()` to observe keyboard show/hide events.
    - Added `keyboardWillShow(sender:)` to adjust the view’s position when a text field is covered by the keyboard.
    - Added `keyboardWillHide()` to reset the view’s position when the keyboard is hidden.
    - Extended `UIResponder` with a `currentFirst()` method to find the current first responder.

--- LoginPageApp/ViewController.swift
+++ LoginPageApp/ViewController.swift
@@ -58,6 +58,7 @@ class ViewController: UIViewController {
         scrollViewSetup()
         loadItems()
         self.dismissKeyboard()
+        self.setupKeyboardHiding()
     }
     
     override func viewDidLayoutSubviews() {
@@ -491,6 +492,36 @@ class ViewController: UIViewController {
         ] )
     }
     
+    func setupKeyboardHiding() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(sender: NSNotification) {
+        
+        guard let userInfo = sender.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+              let currentTextField = UIResponder.currentFirst() as? UITextField else { return }
+        
+        let keyboardTopY = keyboardFrame.cgRectValue.origin.y
+        let convertedTextFieldFrame = view.convert(currentTextField.frame, from: currentTextField.superview)
+        let textFieldBottomY = convertedTextFieldFrame.origin.y + convertedTextFieldFrame.size.height
+
+        if textFieldBottomY > keyboardTopY {
+            let textBoxY = convertedTextFieldFrame.origin.y
+            let newFrameY = (textBoxY - keyboardTopY / 2)
+            view.frame.origin.y = -newFrameY
+        }
+
+    }
+    
+    @objc func keyboardWillHide() {
+        
+        view.frame.origin.y = 0
+    }
+
+    
     @objc func changePassVisibility() {
         
         passField.isSecureTextEntry = !passField.isSecureTextEntry
@@ -525,7 +556,6 @@ class ViewController: UIViewController {
         if self.passField.isEditing {
             
             passBottomLine.backgroundColor = UIColor(red: 1, green: 67 / 255, blue: 42 / 255, alpha: 1).cgColor
-            self.scrollView.setContentOffset(CGPoint(x: 0, y: self.registerButton.frame.maxX), animated: true)
         } else {
             
             passBottomLine.backgroundColor = UIColor.black.cgColor
@@ -596,5 +626,24 @@ extension UIViewController {
     
     @objc func dismissKeyboardTouchOutside() {
         view.endEditing(true)
+    }
+}
+
+extension UIResponder {
+
+    private struct Static {
+        static weak var responder: UIResponder?
+    }
+
+    /// Finds the current first responder
+    /// - Returns: the current UIResponder if it exists
+    static func currentFirst() -> UIResponder? {
+        Static.responder = nil
+        UIApplication.shared.sendAction(#selector(UIResponder._trap), to: nil, from: nil, for: nil)
+        return Static.responder
+    }
+
+    @objc private func _trap() {
+        Static.responder = self
     }
 }
